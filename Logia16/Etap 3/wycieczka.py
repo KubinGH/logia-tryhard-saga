8cc31beb1e6ad932f5904d0ff9940fc8169b185f
KubinGH <KubinGH@users.noreply.github.com>
2018-03-05 18:16:39 +0100

    2018-03-05

--- Logia16/Etap 3/wycieczka.py
+++ Logia16/Etap 3/wycieczka.py
@@ -1,18 +1,42 @@
-# Oh for fuck's sake
+from collections import defaultdict
+def opis(first_visit, second_or_none):
+    if not first_visit or not second_or_none:
+        return []
+    index = 0
+    current_city = first_visit[0]
+    visited = {current_city}
+    graph = defaultdict(list)
+    for city in first_visit[1:]:
+        graph[current_city].append(city)
+        visited.add(current_city); visited.add(city)
+        if city != second_or_none[index]:
+            current_city = city
+        else:
+            for second_city in second_or_none[index+1:]:
+                index += 1
+                if second_city in visited:
+                    current_city = second_city
+                else:
+                    break
 
-def opis(second_or_last, last):
-    n = len(last)
-    j = 0
-    tree = {k: [None, None] for k in last}
-    def firstempty(x): return x.index(None)
-    parent_flag = False
-    for i in range(n):
-        a, b = last[i], second_or_last[j]
-        if a == b: # Leaf
-            parent_flag = True
-            j += 1
+    root = first_visit[0]
+    def traveller(current=root, graph=graph):
+        diary = []
+        diary.append(current)
+        if graph[current]:
+            for neighbour in graph[current]:
+                diary.extend(traveller(neighbour))
+                diary.append(current)
+        return diary
 
-    for k, v in tree.items():
-        print(k, v)
+    full_diary = traveller()
+    last_visit = []
+    for city in full_diary[::-1]:
+        if len(last_visit) == len(visited):
+            break
+        else:
+            if city not in last_visit:
+                last_visit.insert(0, city)
 
-print(opis([4,2,1,3,5,6],[1,2,3,4,6,5]))
+    return last_visit
+    
